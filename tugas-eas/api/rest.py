71af63823ceb0a69e86226934d35c0ab5aa629a3
Ferdinand Jason G <ferdinandjasong@gmail.com>
2019-12-08 16:08:02 +0700

    Update data.sql, Add index, Update api, Update gambar

--- tugas-eas/api/rest.py
+++ tugas-eas/api/rest.py
@@ -5,12 +5,6 @@ from flask import jsonify, request
 
 app = Flask(__name__)
 app.secret_key = "BEDETE"
-app.config['MYSQL_HOST'] = '192.168.16.33'
-app.config['MYSQL_PORT'] = 4000
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'news'
-
 db = mysql.connector.connect(
   host="192.168.16.33",
   user="root",
@@ -19,27 +13,6 @@ db = mysql.connector.connect(
   port=4000
 )
 
-@app.route('/news/c', methods=['GET'])
-def by_title():
-    cur = db.cursor()
-    
-    cur.execute("SELECT * FROM data WHERE category=\"BEDETE\"")
-
-    data = []
-    for (category, headline, authors, link, short_description, date) in cur:
-        temp = {}
-        temp["category"] = category;
-        temp["headline"] = headline;
-        temp["authors"] = authors;
-        temp["link"] = link;
-        temp["short_description"] = short_description;
-        temp["date"] = date;
-        data.append(temp)
-
-    db.commit()
-    cur.close()
-    resp = json.dumps(data)
-    return resp
 
 @app.route('/news', methods=['GET'])
 def get_news():
@@ -47,19 +20,42 @@ def get_news():
     cur.execute("SELECT * FROM data")
 
     data = []
-    for (category, headline, authors, link, short_description, date) in cur:
+    for (news_id, category, headline, authors, link, short_description, date) in cur:
         temp = {}
+        temp["id"] = news_id
         temp["category"] = category;
         temp["headline"] = headline;
         temp["authors"] = authors;
         temp["link"] = link;
         temp["short_description"] = short_description;
-        temp["date"] = date;
+        temp["date"] = date.strftime("%Y-%m-%d");
         data.append(temp)
 
     db.commit()
     cur.close()
     resp = json.dumps(data)
+    return resp
+
+@app.route('/news/<news_id>', methods=['GET'])
+def get_one_news(news_id):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM data WHERE id={}".format(news_id))
+
+    data = []
+    for (news_id, category, headline, authors, link, short_description, date) in cur:
+        temp = {}
+        temp["id"] = news_id
+        temp["category"] = category;
+        temp["headline"] = headline;
+        temp["authors"] = authors;
+        temp["link"] = link;
+        temp["short_description"] = short_description;
+        temp["date"] = date.strftime("%Y-%m-%d");
+        data.append(temp)
+
+    db.commit()
+    cur.close()
+    resp = json.dumps(data[0])
     return resp
 
 @app.route('/news', methods=['POST'])
@@ -74,16 +70,17 @@ def add_news():
 
     
     cur = db.cursor()
-    cur.execute("INSERT INTO data VALUES (%s, %s, %s, %s, %s, %s)", (news_category, news_headline, news_authors, news_link, news_short_description, news_date));
+    cur.execute("INSERT INTO data (`category`, `headline`, `authors`, `link`, `short_description`, `date`) VALUES (%s, %s, %s, %s, %s, %s)", (news_category, news_headline, news_authors, news_link, news_short_description, news_date));
+    news_id = cur.lastrowid
     db.commit()
     cur.close()
 
-    resp = jsonify('News added successfully!')
+    resp = jsonify('News with id = {} added successfully!'.format(news_id))
     resp.status_code = 200
     return resp
 
-@app.route('/news/<id>',methods=['PUT'])
-def update_news(id):
+@app.route('/news/<news_id>',methods=['PUT'])
+def update_news(news_id):
     request_json = request.json
     news_category = request_json["category"]
     news_headline = request_json["headline"]
@@ -93,7 +90,7 @@ def update_news(id):
     news_date = request_json["date"]
 
     cur = db.cursor()
-    cur.execute("UPDATE data SET date = %s WHERE category = %s AND headline = %s AND authors = %s AND link = %s AND short_description = %s", (news_date, news_category, news_headline, news_authors, news_link, news_short_description));
+    cur.execute("UPDATE data SET date = %s, category = %s, headline = %s, authors = %s, link = %s, short_description = %s WHERE id=%s", (news_date, news_category, news_headline, news_authors, news_link, news_short_description, news_id));
     db.commit()
     cur.close()
 
@@ -101,8 +98,8 @@ def update_news(id):
     resp.status_code = 200
     return resp
 
-@app.route('/news/<id>', methods=['DELETE'])
-def delete_news(id):
+@app.route('/news/<news_id>', methods=['DELETE'])
+def delete_news(news_id):
     request_json = request.json
     news_category = request_json["category"]
     news_headline = request_json["headline"]
@@ -112,7 +109,7 @@ def delete_news(id):
     news_date = request_json["date"]
 
     cur = db.cursor()
-    cur.execute("DELETE FROM data WHERE category = %s AND headline = %s AND authors = %s AND link = %s AND short_description = %s AND date = %s", (news_category, news_headline, news_authors, news_link, news_short_description, news_date));
+    cur.execute("DELETE FROM data WHERE id={}".format(news_id));
     db.commit()
     cur.close()
 
